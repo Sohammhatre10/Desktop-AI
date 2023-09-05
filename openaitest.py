4f03f9edd1eba16200e20d991d7df92b65ff931e
Soham Mhatre <106436641+Sohammhatre10@users.noreply.github.com>
2023-09-05 23:27:34 +0530

    Update openaitest.py

--- openaitest.py
+++ openaitest.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.api_key = os.getenv("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")
+openai.api_key = os.getenv("your api-key here")
 
 response = openai.Completion.create(
   model="text-davinci-003",
